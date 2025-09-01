6831d5f06e8e18e47cb5ff1e3571fdc9d834e616
Yigit Erzen <yigterzen@gmail.com>
2025-09-01 14:40:07 +0300

    Implement answer shuffling in QuestionsScreen and update answer handling

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -13,9 +13,17 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
   @override 
   Widget build(context){
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -33,8 +41,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer){
-            return AnswerButton(answerText: answer, onTap: () {});
+          ...currentQuestion.getShuffledAnswers().map((answer){
+            return AnswerButton(answerText: answer, onTap: answerQuestion,);
           })
           ],
         ),
