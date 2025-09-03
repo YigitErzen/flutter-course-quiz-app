b8a8dbb3b51c49c3f81d54b84708b48a8d426feb
Yigit Erzen <yigterzen@gmail.com>
2025-09-03 17:24:24 +0300

    Refactor answer handling logic and import questions data

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
-
+import 'package:quiz_app/data/questions.dart';
 class Quiz extends StatefulWidget {
     const Quiz({super.key});
 
@@ -12,7 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
  class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen= 'start-screen';
 
   void switchScreen(){
@@ -23,6 +23,12 @@ class Quiz extends StatefulWidget {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length){
+      setState((){
+        selectedAnswers=[];
+        activeScreen= 'start-screen';
+      });
+    }
   }
   
   @override
